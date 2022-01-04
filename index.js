d559dd5ed0f3f029d6ae92d79a51748c8cb6e8c0
unknown <alamin8373@gmail.com>
2022-01-04 22:36:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
+const Objectid = require('mongodb').ObjectId;
+
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://table:sjUrHIV50nTchzBY@cluster0.jam0x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("tableData");
+        const dataCollection = database.collection("data");
+
+        app.get('/data', async (req, res) => {
+            const users = dataCollection.find({})
+            const result = await users.toArray();
+            res.send(result)
+        })
+
+        //Delete api
+        app.delete('/data/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: Objectid(id) };
+            const result = await dataCollection.deleteOne(query);
+            res.send(result)
+        });
+
+
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Welcome My server site!')
+})
+
+app.listen(port, () => {
+    console.log(` listening at ${port}`)
+})
